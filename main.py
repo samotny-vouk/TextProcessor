d8eb6d342a5d2e549f110eaf370a47d0936cac3c
samotny-vouk <147575490+samotny-vouk@users.noreply.github.com>
2024-07-25 04:46:39 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QAction, QMenu, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QAction, QMenu, QFileDialog, QInputDialog, \
+    QColorDialog, QFontDialog
 from PyQt5.QtGui import QFont, QTextCharFormat
 
 
@@ -41,6 +42,34 @@ class MainWindow(QMainWindow):
         self.italicAction.triggered.connect(self.setItalic)
         self.formatMenu.addAction(self.italicAction)
 
+        self.underlineAction = QAction("&Подчеркивание", self)
+        self.underlineAction.triggered.connect(self.setUnderline)
+        self.formatMenu.addAction(self.underlineAction)
+
+        self.fontAction = QAction("&Шрифт", self)
+        self.fontAction.triggered.connect(self.changeFont)
+        self.formatMenu.addAction(self.fontAction)
+
+        self.fontSizeAction = QAction("&Размер шрифта", self)
+        self.fontSizeAction.triggered.connect(self.changeFontSize)
+        self.formatMenu.addAction(self.fontSizeAction)
+
+        self.textColorAction = QAction("&Цвет текста", self)
+        self.textColorAction.triggered.connect(self.changeTextColor)
+        self.formatMenu.addAction(self.textColorAction)
+
+        self.indentAction = QAction("&Отступ", self)
+        self.indentAction.triggered.connect(self.applyIndent)
+        self.formatMenu.addAction(self.indentAction)
+
+        self.outdentAction = QAction("&Убрать отступ", self)
+        self.outdentAction.triggered.connect(self.applyOutdent)
+        self.formatMenu.addAction(self.outdentAction)
+
+        self.lineSpacingAction = QAction("&Интервал между строками", self)
+        self.lineSpacingAction.triggered.connect(self.changeLineSpacing)
+        self.formatMenu.addAction(self.lineSpacingAction)
+
         # Создание текстового поля
         self.textEdit = QTextEdit(self)
         self.setCentralWidget(self.textEdit)
@@ -78,6 +107,47 @@ class MainWindow(QMainWindow):
         fmt = QTextCharFormat()
         fmt.setFontItalic(True)
         cursor.mergeCharFormat(fmt)
+
+    def setUnderline(self):
+        self.applyFormat(QTextCharFormat.UnderlineStyle, QTextCharFormat.UnderlineStyle.SingleUnderline)
+
+    def changeFontSize(self):
+        fontSize, ok = QInputDialog.getInt(self, "Размер шрифта", "Введите размер:", 10, 1, 100)
+        if ok:
+            cursor = self.textEdit.textCursor()
+            format = cursor.charFormat()
+            font = QFont(format.font())
+            font.setPointSize(fontSize)
+            format.setFont(font)
+            cursor.mergeCharFormat(format)
+            self.textEdit.setTextCursor(cursor)
+
+    def changeFont(self):
+        font, ok = QFontDialog.getFont()
+        if ok:
+            cursor = self.textEdit.textCursor()
+            format = cursor.charFormat()
+            format.setFont(font)
+            cursor.setCharFormat(format)
+            self.textEdit.setTextCursor(cursor)
+
+    def changeTextColor(self):
+        color = QColorDialog.getColor(self.textEdit.textColor())
+        if color.isValid():
+            self.textEdit.setTextColor(color)
+
+    def applyIndent(self):
+        self.applyFormat(QTextCharFormat.BlockIndent, self.textEdit.textCursor().blockFormat().indent() + 1)
+
+    def applyOutdent(self):
+        self.applyFormat(QTextCharFormat.BlockIndent, self.textEdit.textCursor().blockFormat().indent() - 1)
+
+    def changeLineSpacing(self):
+        spacing, ok = QInputDialog.getDouble(self, "Интервал между строками", "Введите интервал:", 1.0, 0.1, 3.0, 2)
+        if ok:
+            self.textEdit.setLineSpacing(spacing)
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
